4f18b0cb50665cc6d1c0fdd9d3b22454465ee326
kje7631 <kje7631@kje7631s-mbp.attlocal.net>
2020-04-16 14:33:57 -0500

    Created angular project and completed the feature one.

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,12 @@
+export class Book 
+{
+    sku: String;
+    name: String;
+    description: String;
+    unitPrice: Number;
+    imageUrl: String;
+    active: Boolean;
+    unitsInStock: Number;
+    createdOn: Date;
+    updatedOn: Date;
+}
--- onlinebookstore/src/main/java/com/angularspring/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/com/angularspring/onlinebookstore/repository/BookRepository.java
@@ -4,6 +4,7 @@
 package com.angularspring.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.angularspring.onlinebookstore.entity.Book;
 
@@ -11,6 +12,7 @@ import com.angularspring.onlinebookstore.entity.Book;
  * @author kje7631
  *
  */
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
